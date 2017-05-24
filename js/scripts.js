67230d515ba9934117bea49514987dd39ebc21ab
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-05-24 22:08:09 +0000

    added triangle area calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-/* This is my firs JavaScript file ;-) */
-// name varible
-var name = prompt('What\'s your name?');
-//alert('Hi, ' + name + ' !');
-console.log('Hi, ' + name + ' !');
+var triangleArea = 0;
+alert('Hi, we gonna calculate triangle area!');
+var a = prompt('Write the length of side a.');
+var h =  prompt('Give me the height of triangle.');
+triangleArea = a*h/2;
+alert('So, the length of side a is ' + a + ', and the height of triangle is ' + h +'.\nLet\'s calculate it!');
+alert('The area of your triangle is ' + triangleArea + '!');
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
